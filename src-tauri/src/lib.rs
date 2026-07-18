4cf3d15f153634d002651f5d6fae4b2e2151fd7a
simmihugs <simon.graetz@gmx.de>
2026-07-18 18:13:57 +0200

    cleaned up

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,10 +7,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-<<<<<<< HEAD
-=======
         .plugin(tauri_plugin_dialog::init())
->>>>>>> 4e28009 (updated to svelte typescript)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-<<<<<<< HEAD
     flushy_lib::run()
-=======
-    jojo_lib::run()
->>>>>>> 4e28009 (updated to svelte typescript)
 }
